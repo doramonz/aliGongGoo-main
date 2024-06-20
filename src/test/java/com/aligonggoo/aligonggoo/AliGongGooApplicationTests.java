74a12d1755213c6ccdcd7a0cc691d519d60a700a
doramonz <whs3867@gmail.com>
2024-06-20 19:46:46 +0900

    Project Init

---
+++ src/test/java/com/aligonggoo/aligonggoo/AliGongGooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aligonggoo.aligonggoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliGongGooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
